4054f2caef80d9b378838c812bdfd9ace30beaf3
matthiaaas <33065597+matthiaaas@users.noreply.github.com>
2021-01-11 18:54:57 +0100

    prettify import statements by prefixing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react'
 import type { AppProps } from 'next/app'
 import { useProvider, useCreateStore } from 'mobx-store-provider'
-import { RootStore } from '../store'
-import GlobalStyle from '../utils/globalStyle'
+import { RootStore } from '@/store'
+import GlobalStyle from '@/utils/globalStyle'
 
 const App = ({ Component, pageProps }: AppProps) => {
   const applicationStore = useCreateStore(() => RootStore)
